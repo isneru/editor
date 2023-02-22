704e3fcc0b1d4ec53b1eb7b0b7a8f244fa354462
isneru <diogoclanogueira@gmail.com>
2023-02-22 16:35:50 +0000

    Update Toast.tsx and globals.css to adjust spinner styling

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -48,7 +48,7 @@ export const ToastProvider = ({ children }: ToastProviderProps) => {
             }}
             className="fixed right-5 flex items-center justify-center gap-2 rounded px-2 py-1">
             <SpinSVG />
-            <span className="animate-pulse">{toast.message}</span>
+            {toast.message}
           </motion.div>
         )}
       </AnimatePresence>
@@ -63,13 +63,7 @@ const SpinSVG = () => (
     stroke="currentColor"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg">
-    <style
-      dangerouslySetInnerHTML={{
-        __html:
-          ".spinner_V8m1{transform-origin:center;animation:spinner_zKoa 2s linear infinite}.spinner_V8m1 circle{stroke-linecap:round;animation:spinner_YpZS 1.5s ease-in-out infinite}@keyframes spinner_zKoa{100%{transform:rotate(360deg)}}@keyframes spinner_YpZS{0%{stroke-dasharray:0 150;stroke-dashoffset:0}47.5%{stroke-dasharray:42 150;stroke-dashoffset:-16}95%,100%{stroke-dasharray:42 150;stroke-dashoffset:-59}}"
-      }}
-    />
-    <g className="spinner_V8m1">
+    <g className="spinner origin-center">
       <circle cx={12} cy={12} r="9.5" fill="none" strokeWidth={2} />
     </g>
   </svg>
