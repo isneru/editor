3723feac418142340c1feaee6deb7960eeb7f6ae
isneru <diogoclanogueira@gmail.com>
2023-02-22 07:38:40 +0000

    Commit: Refactor Editor.helper.tsx and add debounced change handler to Editor.tsx

--- src/components/Editor/Editor.helper.tsx
+++ src/components/Editor/Editor.helper.tsx
@@ -1,17 +1,21 @@
+import debounce from "lodash.debounce"
 import {
   HTMLAttributes,
   KeyboardEvent,
   ReactNode,
+  useCallback,
   useEffect,
   useRef,
   useState
 } from "react"
+import { api } from "utils/api"
 
-export const EditorHelper = () => {
+interface EditorHelperProps {
+  refetchUserNotes: () => Promise<void>
+}
+
+export const EditorHelper = ({ refetchUserNotes }: EditorHelperProps) => {
   const [lineList, setLineList] = useState<ReactNode[]>([])
-
-  const inputRef = useRef<HTMLParagraphElement>(null)
-
   const linesProps: HTMLAttributes<HTMLHeadingElement | HTMLParagraphElement> =
     {
       contentEditable: true,
@@ -19,11 +23,10 @@ export const EditorHelper = () => {
       suppressContentEditableWarning: true,
       tabIndex: 0
     }
-
+  const inputRef = useRef<HTMLParagraphElement>(null)
   useEffect(() => {
     inputRef.current?.focus()
   }, [lineList])
-
   function handleKeyPress(e: KeyboardEvent<HTMLHeadingElement>) {
     if (e.key === "Enter") {
       e.preventDefault()
@@ -42,8 +45,22 @@ export const EditorHelper = () => {
     }
   }
 
+  const noteNameChange = api.note.changeName.useMutation()
+  async function handleNoteNameChange(noteName: string, noteId: string) {
+    await noteNameChange.mutateAsync({
+      name: noteName,
+      noteId: noteId
+    })
+    await refetchUserNotes()
+  }
+  const debouncedChangeHandler = useCallback(
+    debounce(handleNoteNameChange, 1000),
+    []
+  )
+
   return {
     lineList,
-    handleKeyPress
+    handleKeyPress,
+    debouncedChangeHandler
   }
 }
--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,7 +1,5 @@
 import { Note } from "@prisma/client"
-import debounce from "lodash.debounce"
-import { Dispatch, SetStateAction, useCallback } from "react"
-import { api } from "utils/api"
+import { Dispatch, SetStateAction } from "react"
 import { EditorHelper } from "./Editor.helper"
 
 interface EditorProps {
@@ -15,21 +13,9 @@ export const Editor = ({
   setSelectedNote,
   refetchUserNotes
 }: EditorProps) => {
-  const { lineList, handleKeyPress } = EditorHelper()
-  const noteNameChange = api.note.changeName.useMutation()
-
-  async function handleNoteNameChange(noteName: string, noteId: string) {
-    await noteNameChange.mutateAsync({
-      name: noteName,
-      noteId: noteId
-    })
-    await refetchUserNotes()
-  }
-
-  const debouncedChangeHandler = useCallback(
-    debounce(handleNoteNameChange, 1000),
-    []
-  )
+  const { lineList, handleKeyPress, debouncedChangeHandler } = EditorHelper({
+    refetchUserNotes
+  })
 
   return (
     <section className="mx-auto h-full w-full max-w-[80vw] pt-8 md:max-w-[40vw]">
