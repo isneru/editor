d491a0212358d7e85cb6460a43d2dab7dc92b61b
isneru <diogoclanogueira@gmail.com>
2023-02-25 12:15:17 +0000

    Add @tailwindcss/typography plugin and Markdown-it dependency

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,4 +1,5 @@
-import { useContext } from "react"
+import md from "markdown-it"
+import { useContext, useState } from "react"
 import { NotesContext, ToastContext } from "utils/providers"
 import { EditorHelper } from "./Editor.helper"
 
@@ -6,6 +7,7 @@ export const Editor = () => {
   const { addToast } = useContext(ToastContext)
   const { selectedNote, setSelectedNote } = useContext(NotesContext)
   const { lineList, handleKeyPress, debouncedChangeHandler } = EditorHelper()
+  const [text, setText] = useState("")
 
   return (
     <section className="mx-auto h-full w-full max-w-[80vw] pt-8 md:max-w-[40vw]">
@@ -19,12 +21,29 @@ export const Editor = () => {
             onChange={e => {
               addToast("Saving")
               debouncedChangeHandler(e.currentTarget.value, selectedNote!.id)
-              setSelectedNote({ ...selectedNote, name: e.currentTarget.value })
+              setSelectedNote({
+                ...selectedNote,
+                name: e.currentTarget.value
+              })
             }}
             className="mb-3 bg-transparent text-3xl font-bold leading-none focus:outline-none"
             placeholder="Untitled"
           />
-          {lineList.map(line => line)}
+          {/* {lineList.map(line => line)} */}
+          <div
+            contentEditable
+            className="prose prose-invert focus:outline-none prose-h1:text-3xl"
+            onInput={e => {
+              setText(e.currentTarget.innerText)
+            }}
+            dangerouslySetInnerHTML={{
+              __html: md({
+                html: true,
+                linkify: true,
+                typographer: true
+              }).render(text)
+            }}
+          />
         </>
       )}
     </section>
