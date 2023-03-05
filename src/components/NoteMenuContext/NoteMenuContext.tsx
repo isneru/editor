614450ec288e8655a6fa9277298926969db41f21
isneru <diogoclanogueira@gmail.com>
2023-03-05 14:41:26 +0000

    Add motion animations to NoteMenuContext component

--- src/components/NoteMenuContext/NoteMenuContext.tsx
+++ src/components/NoteMenuContext/NoteMenuContext.tsx
@@ -1,7 +1,8 @@
 import { Note } from "@prisma/client"
 import * as ContextMenu from "@radix-ui/react-context-menu"
 import { TrashIcon } from "@radix-ui/react-icons"
-import { ReactNode, useContext } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+import { ReactNode, useContext, useState } from "react"
 import { NotesContext } from "utils/providers"
 
 interface NoteMenuContextProps {
@@ -10,19 +11,31 @@ interface NoteMenuContextProps {
 }
 
 export const NoteMenuContext = ({ children, note }: NoteMenuContextProps) => {
+  const [open, setOpen] = useState<boolean>(false)
   const { deleteNote } = useContext(NotesContext)
   return (
-    <ContextMenu.Root>
+    <ContextMenu.Root onOpenChange={setOpen}>
       <ContextMenu.Trigger asChild>{children}</ContextMenu.Trigger>
-      <ContextMenu.Content className="rounded-lg bg-background-800 p-1 shadow-[0px_0px_0px_1px] shadow-black/10">
-        <ContextMenu.Item asChild>
-          <button
-            className="flex items-center justify-center gap-2 rounded px-2 py-1 transition-colors hover:bg-white/10"
-            onClick={() => deleteNote(note)}>
-            Delete Note <TrashIcon width={16} height={16} />
-          </button>
-        </ContextMenu.Item>
-      </ContextMenu.Content>
+      <AnimatePresence>
+        {open && (
+          <ContextMenu.Content forceMount asChild>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.08 }}
+              className="rounded-lg bg-background-800 p-1 shadow-[0px_0px_0px_1px] shadow-black/10">
+              <ContextMenu.Item asChild>
+                <button
+                  className="flex items-center justify-center gap-2 rounded px-2 py-1 transition-colors hover:bg-white/10"
+                  onClick={() => deleteNote(note)}>
+                  Delete Note <TrashIcon width={16} height={16} />
+                </button>
+              </ContextMenu.Item>
+            </motion.div>
+          </ContextMenu.Content>
+        )}
+      </AnimatePresence>
     </ContextMenu.Root>
   )
 }
